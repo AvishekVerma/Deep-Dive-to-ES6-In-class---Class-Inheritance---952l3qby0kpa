1930e879744f27163abdca99253b16a0b3b3ceed
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2021-11-23 00:17:55 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
+class Shape {
+    constructor() {  
+        this.color = "blue";    
+      
+    }
+    
+    // Method
+    calculateArea () { 
 
+    }
+
+    drawShape () {
+
+    } 
+  
+  }
 // export class using module.exports
+
+module.exports = Shape;
